100761f220644245008552392b106b066b85fed8
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-06-28 17:05:17 +0300

    added inserts for user

---
+++ database/inserts/user/user.sql
@@ -0,0 +1,10 @@
+USE [CookHubDB];
+
+INSERT INTO [dbo].[User]([Login], [Email], [PhoneNumber], [RoleId], [Password])
+VALUES ('Recardo', 'milos1488@gmail.com', '+1447602408', 3, 'flexflexflex')
+
+INSERT INTO [dbo].[User]([Login], [Email], [PhoneNumber], [RoleId], [Password])
+VALUES ('Moderilla', 'deleteall@gmail.com', '+375291566730', 3, 'icandeleteyou')
+
+INSERT INTO [dbo].[User]([Login], [Email], [PhoneNumber], [RoleId], [Password])
+VALUES ('Nikos', 'nikostop@mail.ru', '+380567835092', 3, '123123123')
