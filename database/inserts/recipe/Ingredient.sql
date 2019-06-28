0002823daa69017119a5be45ef29e9d3d7ee4826
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-06-28 17:45:59 +0300

    Added insert queries for recipe

---
+++ database/inserts/recipe/Ingredient.sql
@@ -0,0 +1,103 @@
+USE [CookHubDB];
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Водка')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Картошка')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Яйцо')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Свинина')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Сыр')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Молоко')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Творог')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Соль')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Сахар')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Перец чёрный')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Перец красный')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Сода')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Вода')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Мука')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Шоколад горький')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Шоколад молочный')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Зелень')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Сливки')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Курица')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Говядина')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Перепилиное яйцо')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Огурец')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Перец сладкий')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Оливки')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Томатная паста')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Маслины')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Салями')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Колбаса охотничья')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Масло сливочное')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Масло подсолнечное')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Понировочные сухари')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Хлеб')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Колбаса докторская')
+
+INSERT INTO [dbo].[Ingredient]([Name])
+VALUES ('Помидор')
---
+++ database/inserts/recipe/ingredient_recipe_details.sql
@@ -0,0 +1,13 @@
+USE [CookHubDB];
+
+INSERT INTO [dbo].[IngredientRecipeDetails]([RecipeId], [IngredientId], [DetailsId])
+VALUES (1, 1, 1)
+
+INSERT INTO [dbo].[IngredientRecipeDetails]([RecipeId], [IngredientId], [DetailsId])
+VALUES (2, 2, 5)
+
+INSERT INTO [dbo].[IngredientRecipeDetails]([RecipeId], [IngredientId], [DetailsId])
+VALUES (3, 3, 6)
+
+INSERT INTO [dbo].[IngredientRecipeDetails]([RecipeId], [IngredientId], [DetailsId])
+VALUES (3, 4, 5)
