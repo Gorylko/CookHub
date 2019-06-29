64a3e3cf8666543fc7cededd314900e0ed86df65
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-06-29 21:13:31 +0300

    Added empty lib for db

---
+++ App/CookHub.Data/DataContext/Realization/MsSqlServer/RecipeContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookHub.Data.DataContext.Realization.MsSqlServer
+{
+    public class RecipeContext
+    {
+    }
+}
