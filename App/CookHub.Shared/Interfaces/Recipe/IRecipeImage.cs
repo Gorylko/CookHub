ab2dfc92cd01cfa440b02c4441004eeeca0a8685
Aldamar <danila.samusev@mail.ru>
2019-06-30 17:47:12 +0300

    All Entities Interfaces have been added

---
+++ App/CookHub.Shared/Interfaces/Recipe/IRecipeImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookHub.Shared.Interfaces.Recipe
+{
+    interface IRecipeImage
+    {
+
+        int Id { get; set; }
+
+        string Path { get; set; }
+
+    }
+}
--- App/CookHub.Shared/Interfaces/User/IUserImage.cs
+++ App/CookHub.Shared/Interfaces/User/IUserImage.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CookHub.Shared.Interfaces
+namespace CookHub.Shared.Interfaces.User
 {
-    class IUser
+    interface IUserImage
     {
+        int Id { get; set; }
+
+        string Path { get; set; }
     }
 }
