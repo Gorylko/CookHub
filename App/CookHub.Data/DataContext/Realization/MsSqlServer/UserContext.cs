7e51b3c9d9d658387aecfd35eb4ed034b2c4f176
Aldamar <danila.samusev@mail.ru>
2019-07-03 20:49:06 +0300

    UserImageContext has been added

--- App/CookHub.Data/DataContext/Realization/MsSqlServer/UserContext.cs
+++ App/CookHub.Data/DataContext/Realization/MsSqlServer/UserContext.cs
@@ -36,7 +36,7 @@ namespace CookHub.Data.DataContext.Realization.MsSqlServer
             using (var connection = new SqlConnection(SqlConst.ConnectionString))
             {
                 var list = new List<IUser>();
-                var command = new SqlCommand("SELECT * FROM [dbo].[User] JOIN [dbo].[Role] on [User.RoleId] = [dbo].[Role.Id]", connection);
+                var command = new SqlCommand("SELECT [dbo].[User] JOIN [dbo].[Role] on [User.RoleId] = [dbo].[Role.Id]", connection);
                 var reader = command.ExecuteReader();
 
                 while (reader.Read())
@@ -52,7 +52,7 @@ namespace CookHub.Data.DataContext.Realization.MsSqlServer
         {
             using (var connection = new SqlConnection(SqlConst.ConnectionString))
             {
-                var command = new SqlCommand("SELECT * FROM [dbo].[User] WHERE [dbo].[User.Id] = @id", connection);
+                var command = new SqlCommand("SELECT [dbo].[User] WHERE [dbo].[User.Id] = @id", connection);
                 command.Parameters.AddWithValue("@id", id);
                 var reader = command.ExecuteReader();
                 reader.Read();
@@ -66,11 +66,13 @@ namespace CookHub.Data.DataContext.Realization.MsSqlServer
             {
                 var command = new SqlCommand("INSERT INTO [dbo].[User]([Id], [Login], [Email], [PhoneNumber], [RoleId]) VALUES (@id, @login, @email, @phoneNumber, @roleId)", connection);
                 //command.Parameters could be one variable: command.Parameters = sqlParameter
+                //if id is long it would be better
                 command.Parameters.AddWithValue("id", user.Id);
                 command.Parameters.AddWithValue("login", user.Id);
                 command.Parameters.AddWithValue("email", user.Email);
                 command.Parameters.AddWithValue("phoneNumber", user.PhoneNumber);
                 command.Parameters.AddWithValue("roleId", (int)user.Role);
+                command.ExecuteNonQuery();
             }
         }
 
---
+++ App/CookHub.Data/DataContext/Realization/MsSqlServer/UserImageContext.cs
@@ -0,0 +1,65 @@
+﻿using CookHub.Data.DataContext.Interfaces;
+using CookHub.Shared.Entities;
+using CookHub.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookHub.Shared.Helpers;
+using SqlConst = CookHub.Data.Constants.SqlConstants;
+using CookHub.Shared.Constants;
+using CookHub.Shared.Entities.Enums;
+
+namespace CookHub.Data.DataContext.Realization.MsSqlServer
+{
+    class UserImageContext
+    {
+        private IUserImage MapUserImage(SqlDataReader reader)
+        {
+            IUserImage userImage = new UserImage
+            {
+                Id = (int)reader["Id"],
+                UserId = (int)reader["UserId"],
+                Path = (string)reader["Path"],        
+            };
+
+            return userImage;
+        }
+
+        public IUserImage GetById(int id)
+        {
+            using (var connection = new SqlConnection(SqlConst.ConnectionString))
+            {
+                var command = new SqlCommand("SELECT [dbo].[UserImage] WHERE [dbo].[UserImage.Id] = @id", connection);
+                command.Parameters.AddWithValue("@id", id);
+                var reader = command.ExecuteReader();
+                reader.Read();
+                return MapUserImage(reader);
+            }
+        }
+
+        public void Save(IUserImage userImage)
+        {
+            using (var connection = new SqlConnection(SqlConst.ConnectionString))
+            {
+                var command = new SqlCommand("INSERT INTO [dbo].[UserImage]([Id], [UserId], [Path]) VALUES (@id, @userId, @path)", connection);             
+                command.Parameters.AddWithValue("id", userImage.Id);
+                command.Parameters.AddWithValue("userId", userImage.UserId);
+                command.Parameters.AddWithValue("path", userImage.Path);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(SqlConst.ConnectionString))
+            {
+                var command = new SqlCommand("DELETE [UserImage] WHERE [Id] = @id");
+                command.Parameters.AddWithValue("@id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
