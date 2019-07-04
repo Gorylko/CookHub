cd2733a68d55b6defc2232e3cdee027e0f21f387
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-07-04 12:17:09 +0300

    Added business lib/ added ingredientService

---
+++ App/CookHub.Business/Services/Recipe/IngredientService.cs
@@ -0,0 +1,36 @@
+﻿using CookHub.Data.DataContext.Interfaces;
+using CookHub.Shared.Interfaces;
+using System.Collections.Generic;
+
+namespace CookHub.Business.Services.Recipe
+{
+    public class IngredientService
+    {
+        private IDataContext<IIngredient> _ingredientContext;
+
+        public IngredientService(IDataContext<IIngredient> context)
+        {
+            this._ingredientContext = context;
+        }
+
+        public IIngredient GetById(int id)
+        {
+            return _ingredientContext.GetById(id);
+        }
+
+        public IReadOnlyCollection<IIngredient> GetAll()
+        {
+            return _ingredientContext.GetAll();
+        }
+
+        public void Detele(int id)
+        {
+            _ingredientContext.Delete(id);
+        }
+
+        public void Save (IIngredient ingredient)
+        {
+            _ingredientContext.Save(ingredient);
+        }
+    }
+}
