8823da251792fa99f1452a326b73951a1444fb16
Gorylko <shamyachka@gmail.com>
2019-08-27 23:37:30 +0300

    removed insert image queries

--- database/startup.bat
+++ database/startup.bat
@@ -25,14 +25,12 @@ sqlcmd -S %server% -i tables\recipe\recipeInfo.sql
 
 sqlcmd -S %server% -i inserts\user\role.sql
 sqlcmd -S %server% -i inserts\user\user.sql
-sqlcmd -S %server% -i inserts\user\userImage.sql
 
 sqlcmd -S %server% -i inserts\recipe\ingredient.sql
 sqlcmd -S %server% -i inserts\recipe\category.sql
 sqlcmd -S %server% -i inserts\recipe\recipe.sql
 sqlcmd -S %server% -i inserts\recipe\units.sql
 sqlcmd -S %server% -i inserts\recipe\recipeInfo.sql
-sqlcmd -S %server% -i inserts\recipe\recipeImage.sql
 
 sqlcmd -S %server% -i functions\func_get_category_by_id.sql
 sqlcmd -S %server% -i functions\func_get_unit_by_id.sql
@@ -44,5 +42,6 @@ sqlcmd -S %server% -i sp\select\sp_select_ingredients_by_recipe_id.sql
 sqlcmd -S %server% -i sp\select\sp_select_images_by_recipe_id.sql
 sqlcmd -S %server% -i sp\select\sp_select_recipe_by_id.sql
 sqlcmd -S %server% -i sp\select\sp_select_recipes.sql
+
 
 pause
