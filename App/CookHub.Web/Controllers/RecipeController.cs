bb6e8ff41a0b04246051a4a0750e6618d7412cf8
Gorylko <shamyachka@gmail.com>
2019-09-05 01:16:19 +0300

    Added react

--- App/CookHub.Web/Controllers/RecipeController.cs
+++ App/CookHub.Web/Controllers/RecipeController.cs
@@ -2,32 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CookHub.Business.Services;
-using CookHub.Business.Services.Interfaces;
-using CookHub.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CookHub.Web.Controllers
 {
+    [Route("api/[controller]")]
     public class RecipeController : Controller
     {
-        private IRecipeService _recipeService;
-
-        public RecipeController(IRecipeService recipeService)
-        {
-            this._recipeService = recipeService;
-        }
-
-        public IActionResult ShowRecipeList()
-        {
-            var model = new RecipeListViewModel { Recipes = _recipeService.GetAll() };
-            return View(model);
-        }
-
-        public IActionResult ShowRecipeInfo(int recipeId)
-        {
-            var model = _recipeService.GetById(recipeId);
-            return View(model);
-        }
     }
 }
--- App/CookHub.Web/Startup.cs
+++ App/CookHub.Web/Startup.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using CookHub.Web.Data;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CookHub.IoC;
-using CookHub.Business.Services.Interfaces;
-using CookHub.Business.Services;
-using CookHub.Web.Dependency;
 
 namespace CookHub.Web
 {
@@ -31,47 +20,47 @@ namespace CookHub.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.Initialize(); //IoC
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseAuthentication();
+            app.UseSpaStaticFiles();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
     }
